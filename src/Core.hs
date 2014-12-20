4aebdfe699e094f66b081b07ef97de89a4790799
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2014-12-20 18:21:18 +0100

    pseudo-static frame initialization from json support

--- src/Core.hs
+++ src/Core.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE BangPatterns               #-}
+{-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase                 #-}
+{-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE PackageImports             #-}
 {-# LANGUAGE TemplateHaskell            #-}
 
@@ -11,35 +14,52 @@ import           Control.Lens
 import           Control.Monad.Reader
 import           Control.Monad.State
 import           Control.Monad.Writer
-import           Criterion.Measurement     as Bench (getTime)
-import           Graphics.Gloss.Data.Color
-import           "GLFW-b" Graphics.UI.GLFW          as GLFW
+import qualified Criterion.Measurement       as Bench (getTime)
+import           Data.Aeson
+import qualified Data.ByteString             as BS
+import qualified Data.ByteString.Char8       as C
+import qualified Data.Map                    as M
+import           Data.Traversable            as T
+import           Data.Vector                 as V (toList)
+import           Graphics.Gloss.Data.Picture
+import qualified "GLFW-b" Graphics.UI.GLFW            as GLFW
 
 data Config = Config
   { _height :: Int
   , _width  :: Int
   , _title  :: String
-  }
+  } deriving (Eq, Show, Read)
 makeLenses ''Config
 
+data UnitType = Player | Ennemy
+  deriving (Eq, Show, Read)
+
+-- Literal sprites are non-loaded sprites
+-- (second member containing path of the picture)
+data SpriteType = SFace | SBack | SRight | SLeft
+  deriving (Eq, Show, Read, Ord)
+type SpritesPaths  = M.Map SpriteType BS.ByteString
+type LoadedSprites = M.Map SpriteType Picture
+
 data Unit = Unit
-  { _x      :: Float
-  , _y      :: Float
-  , _size   :: Float
-  , _speed  :: Float
-  , _ucolor :: Color
-  }
+  { _type    :: UnitType
+  , _x       :: Float
+  , _y       :: Float
+  , _size    :: Float
+  , _speed   :: Float
+  , _sprites :: Either SpritesPaths LoadedSprites
+  } deriving (Eq, Show)
 makeLenses ''Unit
 
 data World = World
   { _units :: [Unit]
-  }
+  } deriving (Eq, Show)
 makeLenses ''World
 
 data Log = Log
   { bfTime :: Double
   , frames :: Int
-  }
+  } deriving (Eq, Show, Read)
 
 instance Monoid Log where
   mempty = Log 0 0
@@ -48,16 +68,9 @@ instance Monoid Log where
 
 newtype Game a =
   Game (ReaderT Config (StateT World (WriterT Log IO)) a) deriving
-      ( MonadIO
-      , Functor
-      , Applicative
-      , Monad
-      , MonadState World
-      , MonadReader Config
-      , MonadWriter Log
+      ( MonadIO, Functor, Applicative, Monad
+      , MonadState World, MonadReader Config, MonadWriter Log
       )
-
--- Util
 
 io :: IO a -> Game a
 io = liftIO
@@ -88,3 +101,36 @@ withWindow f = whenM (io GLFW.init) $ do
     io GLFW.terminate
     where
       simpleErrorCallback e s = putStrLn . unwords $ [show e, show s]
+
+-- Data parsing
+
+instance FromJSON World where
+  parseJSON (Object v) = World <$> v .: "units"
+  parseJSON _ = fail "World parsing failed"
+
+instance FromJSON Unit where
+  parseJSON (Object v) =
+    Unit <$> v .: "type"
+         <*> v .: "x"
+         <*> v .: "y"
+         <*> v .: "size"
+         <*> v .: "speed"
+         <*> (Left <$> v .: "sprites")
+  parseJSON _ = fail "Unit parsing failed"
+
+instance FromJSON SpritesPaths where
+  parseJSON (Array a) = M.fromList . V.toList <$> T.for a
+    (\case
+      (Object v) ->
+        (,) <$> v .: "type" <*> (C.pack <$> v .: "src")
+      _ -> fail "Sprites parsing failed"
+    )
+  parseJSON _ = fail "Sprites parsing failed"
+
+instance FromJSON UnitType where
+  parseJSON (String s) = return . read . show $ s
+  parseJSON _ = fail "UnitType parsing failed"
+
+instance FromJSON SpriteType where
+  parseJSON (String s) = return . read . show $ s
+  parseJSON _ = fail "SpriteType parsing failed"
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE BangPatterns   #-}
+{-# LANGUAGE LambdaCase     #-}
 {-# LANGUAGE PackageImports #-}
 
 module Main where
@@ -10,17 +11,20 @@ import           Control.Monad
 import           Control.Monad.Writer
 import           Core
 import           Criterion.Measurement       (initializeTime)
-import           Data.Foldable               as F
-import           Data.Map                    as M hiding (size)
-import           Data.Traversable            as T
+import           Data.Aeson (eitherDecode)
+import qualified Data.ByteString.Lazy        as BS
+import qualified Data.Foldable               as F
+import qualified Data.Map                    as M hiding (size)
+import qualified Data.Traversable            as T
 import           FRP.Elerea.Simple
 import           Graphics.Gloss.Data.Color
 import           Graphics.Gloss.Data.Picture
 import           Graphics.Gloss.Rendering
 import           "GLFW-b" Graphics.UI.GLFW            as GLFW
 import           Logic
+import qualified Data.ByteString.Char8       as C
 
-type Keymap = Map Key (Game ())
+type Keymap = M.Map Key (Game ())
 
 defaultKeymap :: Keymap
 defaultKeymap = M.fromList
@@ -37,33 +41,34 @@ defaultConf = Config
   , _title     = "test"
   }
 
-defaultWorld :: World
-defaultWorld = World
-  { _units =
-      [ Unit 0 (-100) 10 4 blue
-      , Unit 0 100 30 3.5 white
-      , Unit 50 50 20 15 green
-      ]
-  }
-
 main :: IO ()
-main = displayLog =<<
-  runGame defaultConf defaultWorld
-    (withWindow
-    $ \w -> do
-      -- Signal network initialization
-      !(netw, snk) <- io $ do
-        (smp, snk) <- external M.empty
-        netw <- start $ return smp
-        return (netw, snk)
+main = liftM eitherDecode (BS.readFile "ressources/world.json")
+  >>= \case
+  (Right world) ->
+    runGame defaultConf world
+      (withWindow
+      $ \w -> do
+        -- Signal network initialization
+        !(netw, snk) <- io $ do
+          (smp, snk) <- external M.empty
+          netw <- start $ return smp
+          return (netw, snk)
 
-      -- glossState initialization
-      !glossState <- io initState
+        -- glossState initialization
+        !glossState <- io initState
 
-      -- begin of the loop
-      io initializeTime
-      loop w (netw, snk) glossState
-  )
+        -- units initialization
+        !initUnits <- use units >>= T.traverse (\u ->
+          case u^.sprites of
+            (Right _) -> fail "Error: sprites already loaded"
+            (Left sp) -> liftM (flip (set sprites) u)
+              $ Right <$> io (initSprites sp))
+        units .= initUnits
+
+        -- begin of the loop
+        io initializeTime
+        loop w (netw, snk) glossState) >>= displayLog
+  (Left e) -> putStrLn e
 
   where
     loop window (netw, snk) glossState = do
@@ -77,11 +82,11 @@ main = displayLog =<<
           (return ())
           (loop window (netw, snk) glossState)
 
-    readInput :: GLFW.Window -> (Map Key (Game ()) -> IO ()) -> IO ()
+    readInput :: GLFW.Window -> (Keymap -> IO ()) -> IO ()
     readInput window snk = do
       pollEvents
       -- todo: dedicated optimized function
-      keysPressed <- filterM (keyIsPressed window) . keys $ defaultKeymap
+      keysPressed <- filterM (keyIsPressed window) . M.keys $ defaultKeymap
       snk . M.filterWithKey (\k _ -> F.elem k keysPressed)
         $ defaultKeymap
 
@@ -97,9 +102,12 @@ main = displayLog =<<
          $ T.fmapDefault displayUnit u
       io $ swapBuffers window
 
-    displayUnit u = Color (u^.ucolor)
+    displayUnit u = Color red
       $ translate (u^.x) (u^.y)
       $ circle (u^.size)
+
+    initSprites :: SpritesPaths -> IO LoadedSprites
+    initSprites = traverse (loadBMP . C.unpack)
 
 displayLog :: Log -> IO ()
 displayLog (Log t f) = do
